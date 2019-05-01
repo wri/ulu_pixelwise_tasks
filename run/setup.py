1f5f54ddfa6854e62048402e3b66854a89361df4
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-05-01 15:23:35 -0700

    add run.setup

---
+++ run/setup.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import warnings
+import run.product as product
+import run.model as model
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def task(*args,**kwargs):
+    job_prod=product.create(*args,**kwargs)    
+    job_bands=product.add_bands(*args,**kwargs)    
+    job_model=model.upload(*args,**kwargs)
+    return [ job_prod, job_bands, job_model ]
+
+
